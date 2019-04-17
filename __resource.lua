e28f2d77a65e35ac5ee9d0fc82b08cf4617a04b8
Mike The Madkiwi <mike@hildyard.ca>
2019-04-17 09:35:03 -0400

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,28 +1,21 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
--- resource_type 'gametype' { name = 'MIAV' }
--- resource_type 'map' { gameTypes = { dev = true } }
-
--- map 'devMap.lua'
-
-description 'NoDB Ban and Client Mitigation Script. V2.'
+description 'Ban and Client Mitigation Script. V2.'
 ui_page 'ui/ui.html'
-
 -- Server
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'config.lua',
 	'Server/svmain.lua'
 }
-
 -- Client
 client_scripts {
 	'Client/clmain.lua'
 }
-
 -- NUI Files
 files {
 	'ui/ui.html',
 }
+-- Dependancies
 dependencies {
 	'mysql-async'
 }
