281590a5b8b17037d184da27b542833b79cf8b34
Mike The Madkiwi <mike@hildyard.ca>
2019-04-20 17:20:39 -0400

    dev

--- __resource.lua
+++ __resource.lua
@@ -1,27 +1,10 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Ban and Client Mitigation Script. V2.'
-ui_page 'ui/ui.html'
 -- Server
 server_scripts {
 	'@async/async.lua',
 	'@mysql-async/lib/MySQL.lua',
-	'Server/svmain.lua',
-	'Server/svevents.lua'
-}
--- Client
-client_scripts {
-	'Client/clmain.lua'
-}
--- NUI Files
-files {
-	'ui/ui.html',
-}
-
-exports {
-	'getSharedObject'
-}
-server_exports {
-	'getSharedObject'
+	'Server/svmain.lua'
 }
 -- Dependancies
 dependencies {
