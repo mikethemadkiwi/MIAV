9d3491d397008e0853f2e78b891320a87f1b9b0a
Mike The Madkiwi <mike@hildyard.ca>
2019-04-21 18:45:44 -0400

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,12 +2,10 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Ban and Client Mitigation Script. V2.'
 -- Server
 server_scripts {
-	'@async/async.lua',
 	'@mysql-async/lib/MySQL.lua',
 	'Server/svmain.lua'
 }
 -- Dependancies
 dependencies {
-	'async',
 	'mysql-async'
 }
